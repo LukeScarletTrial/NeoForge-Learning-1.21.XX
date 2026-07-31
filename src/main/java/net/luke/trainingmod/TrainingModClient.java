c41f7a9ec7a3ba1ce6aea3bb90652e9cdc684506
LukeScarletTrial <luke.rifty@gmail.com>
2026-07-31 01:31:36 +0100

    PUSH V2

--- src/main/java/net/luke/trainingmod/TrainingModClient.java
+++ src/main/java/net/luke/trainingmod/TrainingModClient.java
@@ -2,23 +2,23 @@ package net.luke.trainingmod;
 
 import net.luke.trainingmod.util.ModItemProperties;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = TrainingMod.MODID, dist = Dist.CLIENT)
-@EventBusSubscriber(modid = TrainingMod.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class TrainingModClient {
-    public TrainingModClient(ModContainer container) {
+    public TrainingModClient(ModContainer container, IEventBus modEventBus) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+
+        // Register the client setup listener directly on the mod event bus
+        modEventBus.addListener(this::onClientSetup);
     }
 
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
+    private void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             ModItemProperties.addCustomItemProperties();
         });
